a90a57291e009050c1c4ac5caa79458e4722ab2f
aldanaturraca <113069488+aldanaturraca@users.noreply.github.com>
2022-10-03 20:47:27 -0300

    Add files via upload

---
+++ inicio.js
@@ -0,0 +1,17 @@
+let edad;
+edad = Number((prompt("Introduce tu edad")));
+if (edad >=18){
+    alert("Puedes entrar, eres mayor de edad");
+} else {
+    alert("Eres menor de edad, puedes entrar bajo tu responsabilidad");
+}
+
+window.onbeforeunload = preguntarAntesDeSalir;
+
+function preguntarAntesDeSalir(){
+return "¿Seguro que quieres salir?";
+}
+
+function preguntarEnvioProducto
+var resultado
+var 
